6baf3f521b53eb2243ea78c7ddfebf1eda284eda
justin <justin@brightal.com>
2010-08-13 18:49:18 +0530

    files remover

--- app/models/member_invitation.rb
+++
@@ -1,2 +0,0 @@
-class MemberInvitation < ActiveRecord::Base
-end
--- db/migrate/20100802104229_addcolumn.rb
+++
@@ -1,9 +0,0 @@
-class Addcolumn < ActiveRecord::Migration
-  def self.up
-    add_column :tasks, :member, :string
-  end
-
-  def self.down
-    remove_column :tasks, :member, :string
-  end
-end
